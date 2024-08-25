e819ad64f13e21e7f898ca644dcfcfa273465c18
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2024-08-25 15:41:08 -0500

    Deletes sprite.constraint package

--- modules/simply-typed/src/main/scala/sprite/constraint/Predicate.scala
+++
@@ -1,9 +0,0 @@
-package sprite.constraint
-
-enum Predicate:
-  case True, False
-  case Var(name: String)
-  case Not(p: Predicate)
-  case And(p1: Predicate, p2: Predicate)
-  case Or(p1: Predicate, p2: Predicate)
-  case UninterpFunction(name: String, arg: Predicate)
