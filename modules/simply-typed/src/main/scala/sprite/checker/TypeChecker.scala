182e92ef5a827136014d19e4adcbe64874beb464
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2024-09-04 21:49:59 -0500

    Defines implicationConstraint

--- modules/simply-typed/src/main/scala/sprite/checker/TypeChecker.scala
+++ modules/simply-typed/src/main/scala/sprite/checker/TypeChecker.scala
@@ -1,8 +1,13 @@
 package sprite.checker
 
+import sprite.checker.TypeSynthetizer.Inferable
 import sprite.language.*
 import sprite.language.SpriteTerm.*
+import sprite.language.SpriteType.RefinedType
+import sprite.languages.Substitution.substitute
 import sprite.solver.qflia.language.Constraint
+import sprite.solver.qflia.language.IntTerm
+import sprite.solver.qflia.substitutions.BoolTermSubstitution
 
 trait TypeChecker:
   synthetizer: TypeSynthetizer =>
@@ -12,8 +17,24 @@ trait TypeChecker:
       term: SpriteTerm,
       expectedType: SpriteType
   ): TypeCheckResult[Constraint] = term match
-    case Lambda(params, body)            => ???
-    case Let(binding, body)              => ???
-    case term: TypeSynthetizer.Inferable => ???
+    case Lambda(params, body) => ???
+
+    case Let(binding, body) => ???
+
+    case term: Inferable => ???
+
+  def implicationConstraint(
+      param: String,
+      paramType: SpriteType,
+      consequent: Constraint
+  ): Constraint = paramType match
+    case RefinedType(b, Refinement(v, p)) =>
+      Constraint.forall(
+        param,
+        antecedent = p.substitute(v, IntTerm.Var(param)),
+        consequent
+      )
+
+    case _ => consequent
 
 object TypeChecker
